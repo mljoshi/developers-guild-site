7042931eba78b4103381e13f5712d9b04fac2100
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-05-01 20:50:42 -0700

    Update frontWebPage.js

--- frontWebPage.js
+++ frontWebPage.js
@@ -1,1 +1,5 @@
-//insert title text here
+//removes the underline from the anchor elements in the header
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.style.textDecoration = 'none';
+}); 
+
